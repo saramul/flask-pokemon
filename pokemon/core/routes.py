436a4579bfa3eb587a42ccd8c9356c8665648f05
saramul <sarasurius@gmail.com>
2026-02-15 21:05:50 +0700

    add pokemon home page

--- pokemon/core/routes.py
+++ pokemon/core/routes.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from pokemon.models import Pokemon, PokemonType, User
+from pokemon.extension import db
 
 core_bp = Blueprint('core', __name__, template_folder='templates')
 
 @core_bp.route('/')
 def index():
-  return render_template('core/index.html', title='Home Page')
+  page = request.args.get('page', type=int)
+  pokemons = db.paginate(db.select(Pokemon), per_page=4, page=page)
+  return render_template('core/index.html', title='Home Page', pokemons=pokemons)
+
+@core_bp.route('/<int:id>/details')
+def pokemon_detail(id):
+  pokemon = db.session.get(Pokemon, id)
+  return render_template('core/pokemon_detail.html', title='Pokemon Detail Page', pokemon=pokemon)
--- pokemon/pokedex/routes.py
+++ pokemon/pokedex/routes.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import current_user, login_required
 from pokemon.extension import db
-from pokemon.models import Pokemon, PokemonType
+from pokemon.models import Pokemon, PokemonType, User
 
 pokedex_bp = Blueprint('pokedex', __name__, template_folder='templates')
 
 @pokedex_bp.route('/')
 @login_required
 def index():
-  query = db.select(Pokemon)
+  query = db.select(Pokemon).where(Pokemon.user_id==current_user.id)
   pokemons = db.session.scalars(query).all()
   return render_template('pokedex/index.html', title='Pokedex Page', pokemons=pokemons)
 
